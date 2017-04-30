8b1088d2ef61055bdd71df25af5e57e6ee6e5d56
Isaac Carrera <ifcarrera@uc.cl>
2017-04-30 20:22:28 -0300

    Fix validations player model

--- app/models/match.rb
+++ app/models/match.rb
@@ -17,6 +17,6 @@
 class Match < ApplicationRecord
   belongs_to :tournament
 
-  belongs_to :equipo_local, :class_name => 'User'
-  belongs_to :equipo_visita, :class_name => 'User'
+  belongs_to :equipo_local, :class_name => 'Team'
+  belongs_to :equipo_visita, :class_name => 'Team'
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -14,12 +14,11 @@
 #  updated_at   :datetime         not null
 #  team_id      :integer
 #
-
 class Player < ApplicationRecord
   belongs_to :team
-  
-  validates :nombre, presence: true, allow_blank: false, format: /\A([a-zA-Z']\S*)\z/
-  validates :apellido, presence: true, allow_blank: false, format: /\A([a-zA-Z']\S*)\z/
+
+  validates :nombre, presence: true, allow_blank: false, format: /\A([A-Za-z']*)\z/
+  validates :apellido, presence: true, allow_blank: false, format: /\A([A-Za-z']*)\z/
   # el formato es para validar que solo tenga caracteres, no numeros, espacios ni caracteres especiales, salvo el apostrofe
   validates :pais, presence: true, allow_blank: false
   validates :team_id, presence: true, allow_blank: false
