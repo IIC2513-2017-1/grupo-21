a2825daa4459870fc4d105330a76ceb7f423b4ed
Isaac Carrera <ifcarrera@uc.cl>
2017-05-14 02:05:55 -0400

    Nested routes team-players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,31 +4,36 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    @team = Team.find(params[:team_id])
+    @players = @team.players
   end
 
   # GET /players/1
   # GET /players/1.json
   def show
+    @team = Team.find(params[:team_id])
   end
 
   # GET /players/new
   def new
+    @team = Team.find(params[:team_id])
     @player = Player.new
   end
 
   # GET /players/1/edit
   def edit
+    @team = Team.find(params[:team_id])
   end
 
   # POST /players
   # POST /players.json
   def create
-    @player = Player.new(player_params)
+    @team = Team.find(params[:team_id])
+    @player = @team.players.new(player_params)
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+        format.html { redirect_to team_players_path(@team), notice: 'Player was successfully created.' }
         format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new }
@@ -42,7 +47,7 @@ class PlayersController < ApplicationController
   def update
     respond_to do |format|
       if @player.update(player_params)
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
+        format.html { redirect_to team_players_path(@team), notice: 'Player was successfully updated.' }
         format.json { render :show, status: :ok, location: @player }
       else
         format.html { render :edit }
@@ -56,7 +61,7 @@ class PlayersController < ApplicationController
   def destroy
     @player.destroy
     respond_to do |format|
-      format.html { redirect_to players_url, notice: 'Player was successfully destroyed.' }
+      format.html { redirect_to tournament_matches_path(@tournament), notice: 'Player was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -65,6 +70,7 @@ class PlayersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_player
       @player = Player.find(params[:id])
+      @team = Team.find(params[:team_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :destroy, :edit, :update, :show]
-  resources :teams
   resources :tournaments do
     resources :matches
   end
-  resources :players
+  resources :teams do
+    resources :players
+  end
   resource :session, only: [:new, :create, :destroy]
   root 'tournaments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
